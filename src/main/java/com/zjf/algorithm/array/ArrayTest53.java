5f753984c27bb4a907ae7f59e4fefe0c46b5d2f6
张建峰 <zhangjianfeng_sl@163.com>
2020-04-22 17:25:34 +0800

    力扣第解法53题

---
+++ src/main/java/com/zjf/algorithm/array/ArrayTest53.java
@@ -0,0 +1,26 @@
+package com.zjf.algorithm.array;
+
+/**
+ * description: ArrayTest53 <br>
+ * date: 2020/4/22 16:23 <br>
+ * author: 张建峰 <br>
+ */
+public class ArrayTest53 {
+
+	/**
+	 * description:
+	 * 给定一个整数数组 nums ，找到一个具有最大和的连续子数组（子数组最少包含一个元素），返回其最大和。
+	 *
+	 * 示例:
+	 * 输入: [-2,1,-3,4,-1,2,1,-5,4],
+	 * 输出: 6
+	 * 解释: 连续子数组 [4,-1,2,1] 的和最大，为 6。
+	*/
+	public int maxSubArray(int[] nums) {
+		return 0;
+	}
+
+	public static void main(String[] args) {
+
+	}
+}
