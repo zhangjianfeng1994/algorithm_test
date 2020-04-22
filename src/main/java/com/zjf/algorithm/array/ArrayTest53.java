b8c951910610d140028f7bc2ef44df5851632a1f
zhangjianfeng <zhangjianfeng_sl@163.com>
2020-04-22 23:45:59 +0800

    53题

--- src/main/java/com/zjf/algorithm/array/ArrayTest53.java
+++ src/main/java/com/zjf/algorithm/array/ArrayTest53.java
@@ -17,10 +17,34 @@ public class ArrayTest53 {
 	 * 解释: 连续子数组 [4,-1,2,1] 的和最大，为 6。
 	*/
 	public int maxSubArray(int[] nums) {
-		return 0;
+		int len = nums.length;
+		if(len == 0){
+			return 0;
+		}
+		if (len == 1){
+			return nums[0];
+		}
+		//标记开始位置
+		int k;
+		//记录总数
+		int sum = nums[0];;
+		for (int i = 1; i < nums.length; i++) {
+			int num = nums[i];
+			if (num >= sum + num){
+				sum = num;
+				k = i;
+			}else {
+				sum += num;
+			}
+		}
+
+		return sum;
 	}
 
 	public static void main(String[] args) {
-
+		int[] nums = {-2,1,-3,4,-1,2,1,-5,4};
+		ArrayTest53 arrayTest53 = new ArrayTest53();
+		int sum = arrayTest53.maxSubArray(nums);
+		System.out.println(sum);
 	}
 }
