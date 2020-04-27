24133683c8835b481670b0f0facb30a6065ad805
张建峰 <zhangjianfeng_sl@163.com>
2020-04-27 16:44:01 +0800

    力扣第解法86题

---
+++ src/main/java/com/zjf/algorithm/array/ListNode.java
@@ -0,0 +1,15 @@
+package com.zjf.algorithm.array;
+
+/**
+ * description: ListNode <br>
+ * date: 2020/4/27 15:12 <br>
+ * author: 张建峰 <br>
+ */
+public class ListNode {
+	int val;
+	ListNode next;
+
+	ListNode(int x) {
+		val = x;
+	}
+}
---
+++ src/main/java/com/zjf/algorithm/array/Test86.java
@@ -0,0 +1,13 @@
+package com.zjf.algorithm.array;
+
+/**
+ * description: Test86 <br>
+ * date: 2020/4/27 14:58 <br>
+ * author: 张建峰 <br>
+ */
+public class Test86 {
+
+	public ListNode partition(ListNode head, int x) {
+		return null;
+	}
+}
