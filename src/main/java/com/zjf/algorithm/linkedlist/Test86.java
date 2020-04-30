7b408ff2a094226e3681e666da430e5df65b0988
zhangjianfeng <zhangjianfeng_sl@163.com>
2020-04-30 19:09:27 +0800

    86题

--- src/main/java/com/zjf/algorithm/linkedlist/Test86.java
+++ src/main/java/com/zjf/algorithm/linkedlist/Test86.java
@@ -12,7 +12,7 @@ public class Test86 {
 	 * 给定一个链表和一个特定值 x，对链表进行分隔，使得所有小于 x 的节点都在大于或等于 x 的节点之前。
 	 * 你应当保留两个分区中每个节点的初始相对位置。
 	 * 示例:
-	 * 输入: head = 1->4->3->2->5->2, x = 3
+	 * 输入: head = 1-2->4->3->2->2, x = 3
 	 * 输出: 1->2->2->4->3->5
 	 */
 	public ListNode partition(ListNode head, int x) {
@@ -42,7 +42,7 @@ public class Test86 {
 				lastSmall = next;
 				curr = curr;
 			}else{
-				curr = curr.next;;
+				curr = curr.next;
 			}
 		}
 		return head;
