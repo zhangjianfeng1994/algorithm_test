6d66b528b22ab230ecdc7e7ea32f6840068af6b6
zhangjianfeng <zhangjianfeng_sl@163.com>
2020-05-08 00:30:50 +0800

    445题

--- src/main/java/com/zjf/algorithm/linkedlist/Test445.java
+++ src/main/java/com/zjf/algorithm/linkedlist/Test445.java
@@ -1,5 +1,7 @@
 package com.zjf.algorithm.linkedlist;
 
+import java.util.Stack;
+
 /**
  * description: Test445 <br>
  * date: 2020/5/6 9:58 <br>
@@ -18,8 +20,52 @@ public class Test445 {
 	 * 输出：7 -> 8 -> 0 -> 7
 	 *
 	*/
+	/**
+	 *
+	 */
+
 	public ListNode addTwoNumbers(ListNode l1, ListNode l2) {
 
-		return null;
+		ListNode node1 = reverse(l1);
+		ListNode node2 = reverse(l2);
+		int carry = 0;
+		ListNode newHead = null;
+		while (node1 != null || node2 != null){
+			int x = node1 != null ? node1.val : 0;
+			int y = node2 != null ? node2.val : 0;
+			int num = (x + y + carry) % 10;
+			ListNode node = new ListNode(num);
+			node.next = newHead;
+			newHead = node;
+			carry = (x + y + carry) / 10;
+			if (node1 != null) {
+				node1 = node1.next;
+			}
+			if (node2 != null) {
+				node2 = node2.next;
+			}
+		}
+		// 最后加上 carry
+		if (carry > 0) {
+			ListNode node = new ListNode(carry);
+			node.next = newHead;
+			newHead = node;
+		}
+		return newHead;
+	}
+
+	// 链表反转
+	public ListNode reverse(ListNode head){
+		if (head == null) {
+			return null;
+		}
+		ListNode pre = null;
+		while (head != null){
+			ListNode next = head.next;
+			head.next = pre;
+			pre = head;
+			head = next;
+		}
+		return pre;
 	}
 }
