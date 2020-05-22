256d1767ec0fb08a57405fc4df8b36419b4d71c6
张建峰 <zhangjianfeng_sl@163.com>
2020-05-22 10:49:54 +0800

    力扣第解法103题

--- src/main/java/com/zjf/algorithm/tree/Test103.java
+++ src/main/java/com/zjf/algorithm/tree/Test103.java
@@ -1,7 +1,9 @@
 package com.zjf.algorithm.tree;
 
 import java.util.ArrayList;
+import java.util.LinkedList;
 import java.util.List;
+import java.util.Queue;
 
 public class Test103 {
 
@@ -24,10 +26,40 @@ public class Test103 {
 	 *   [15,7]
 	 * ]
 	 */
+	/**
+	 * description:
+	 * 执行用时 :1 ms, 在所有 Java 提交中击败了97.74%的用户
+	 * 内存消耗 :39.5 MB, 在所有 Java 提交中击败了7.41%的用户
+	*/
 	public List<List<Integer>> zigzagLevelOrder(TreeNode root) {
 		List<List<Integer>> res = new ArrayList<List<Integer>>();
 		if(root == null){
 			return  res;
+		}
+		Queue<TreeNode>  queue = new LinkedList<TreeNode>();
+		//记录深度
+		int depth = 1;
+		queue.offer(root);
+		while (!queue.isEmpty()){
+			int size = queue.size();
+			LinkedList<Integer> depthList = new LinkedList<Integer>();
+			for (int i = 0; i < size; i++) {
+				TreeNode node = queue.poll();
+				//深度(行数)是奇数时
+				if (depth%2>0){
+					depthList.add(node.val);
+				}else{
+					depthList.addFirst(node.val);
+				}
+				if (node.left != null){
+					queue.offer(node.left);
+				}
+				if (node.right != null){
+					queue.offer(node.right);
+				}
+			}
+			depth++;
+			res.add(depthList);
 		}
 		return  res;
 	}
