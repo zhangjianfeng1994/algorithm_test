8d102872b0b33d2a22532249e6c6ef1a2c8ceb23
zhangjianfeng <zhangjianfeng_sl@163.com>
2020-06-05 23:37:22 +0800

    114题

---
+++ src/main/java/com/zjf/algorithm/tree/Test114.java
@@ -0,0 +1,30 @@
+package com.zjf.algorithm.tree;
+
+public class Test114 {
+
+	/**
+	 * 给定一个二叉树，原地将它展开为一个单链表。
+	 * 例如，给定二叉树
+	 *
+	 *     1
+	 *    / \
+	 *   2   5
+	 *  / \   \
+	 * 3   4   6
+	 * 将其展开为：
+	 * 1
+	 *  \
+	 *   2
+	 *    \
+	 *     3
+	 *      \
+	 *       4
+	 *        \
+	 *         5
+	 *          \
+	 *           6
+	 */
+	public void flatten(TreeNode root) {
+
+	}
+}
