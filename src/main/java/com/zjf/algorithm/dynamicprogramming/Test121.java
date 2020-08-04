82128d18ac0f9c3335a082643c74e7e9a9b76347
zhangjianfeng <zhangjianfeng_sl@163.com>
2020-08-05 00:36:08 +0800

    stack 121 122 动态规划  股票最佳收益系列问题

--- src/main/java/com/zjf/algorithm/dynamicprogramming/Test121.java
+++ src/main/java/com/zjf/algorithm/dynamicprogramming/Test121.java
@@ -14,12 +14,14 @@ public class Test121 {
 	 * 示例 1:
 	 * 输入: [7,1,5,3,6,4]
 	 * 输出: 5
+	 *  4 -2 3 = 5    6-1 = 5-1 + 3-5 + 6-3
 	 * 解释: 在第 2 天（股票价格 = 1）的时候买入，在第 5 天（股票价格 = 6）的时候卖出，最大利润 = 6-1 = 5 。
 	 *      注意利润不能是 7-1 = 6, 因为卖出价格需要大于买入价格；同时，你不能在买入前卖出股票。
 	 * 示例 2:
 	 * 输入: [7,6,4,3,1]
 	 * 输出: 0
 	 * 解释: 在这种情况下, 没有交易完成, 所以最大利润为 0。
+	 * dp[n] =
 	 */
 	public int maxProfit(int[] prices) {
 		if(prices == null || prices.length == 0 ||prices.length == 1){
@@ -27,21 +29,28 @@ public class Test121 {
 		}
 		int[] dp = new int[prices.length];
 		for (int i = 0; i < prices.length-1; i++) {
-			for (int j = i+1; j < prices.length; j++) {
-				dp[i] = Math.max(dp[i],prices[j]-prices[i]);
+			dp[i] = prices[i+1]-prices[i];
+		}
+		//记录当前值的总和
+		int currSum = dp[0];
+		//记录最大值
+		int maxSum = dp[0];
+		for (int i = 1; i < dp.length; i++) {
+			int num = dp[i];
+			//状态转移方程
+			if (currSum >= 0){
+				currSum += num;
+			}else {
+				currSum = num;
 			}
+			maxSum = Math.max(currSum,maxSum);
 		}
-		int result = Integer.MIN_VALUE;
-		for (int i = 0; i < dp.length; i++) {
-			int i1 = dp[i];
-			result = Math.max(i1,result);
-		}
-		return result;
+		return maxSum;
 	}
 
 	public static void main(String[] args) {
 		Test121 test = new Test121();
-		int[] prices = {7,1,5,3,7,4};
+		int[] prices = {7,1,5,3,6,4};
 		System.out.println(test.maxProfit(prices));
 	}
 }
