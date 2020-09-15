80e7cad47abf48985b6fc7c54481c85fd694a98a
zhangjianfeng1994 <736391262@qq.com>
2020-09-15 18:36:34 +0800

    力扣 5

---
+++ src/main/java/com/zjf/algorithm/dynamicprogramming/Test5.java
@@ -0,0 +1,23 @@
+package com.zjf.algorithm.dynamicprogramming;
+
+/**
+ * description: Test5 <br>
+ * date: 2020/9/15 17:36 <br>
+ * author: 张建峰 <br>
+ */
+public class Test5 {
+
+	/**
+	 * 给定一个字符串 s，找到 s 中最长的回文子串。你可以假设 s 的最大长度为 1000。
+	 * 示例 1：
+	 * 输入: "babad"
+	 * 输出: "bab"
+	 * 注意: "aba" 也是一个有效答案。
+	 * 示例 2：
+	 * 输入: "cbbd"
+	 * 输出: "bb"
+	*/
+	public String longestPalindrome(String s) {
+
+	}
+}
