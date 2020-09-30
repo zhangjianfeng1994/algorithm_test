2932a97a6dba19ddf29b7cdbc7f6ff1b43f76a50
张建峰 <736391262@qq.com>
2020-09-30 11:50:26 +0800

    力扣 28 KMP

--- src/main/java/com/zjf/algorithm/dynamicprogramming/Test28_KMP.java
+++ src/main/java/com/zjf/algorithm/dynamicprogramming/Test28_KMP.java
@@ -25,6 +25,22 @@ public class Test28_KMP {
 	 * 当 needle 是空字符串时，我们应当返回什么值呢？这是一个在面试中很好的问题。
 	 */
 	public int strStr(String haystack, String needle) {
-		return 0;
+
+		int m = haystack.length(), n = needle.length();
+		if (n == 0) return 0;
+		for (int i = 0; i <= m - n; i++) {
+			for (int j = 0; j < n; j++) {
+				if (haystack.charAt(i + j) != needle.charAt(j)) break;
+				if (j == n - 1) return i;
+			}
+		}
+		return -1;
+	}
+
+	public static void main(String[] args) {
+		Test28_KMP test = new Test28_KMP();
+		String haystack = "mississippi";
+		String needle = "issip";
+		System.out.println(test.strStr(haystack,needle));
 	}
 }
