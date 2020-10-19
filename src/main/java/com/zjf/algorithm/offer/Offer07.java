0e2088f04078f172d0694db3acaf65ba947130ac
zhangjianfeng <zhangjianfeng_sl@163.com>
2020-10-19 22:55:44 +0800

    剑指offer07

---
+++ src/main/java/com/zjf/algorithm/offer/Offer07.java
@@ -0,0 +1,28 @@
+package com.zjf.algorithm.offer;
+
+/**
+ * @Description :
+ * @Author : ZJF
+ * @Date: 2020-10-19 22:28  //时间
+ */
+public class Offer07 {
+
+	/**
+	 * 输入某二叉树的前序遍历和中序遍历的结果，请重建该二叉树。
+	 * 假设输入的前序遍历和中序遍历的结果中都不含重复的数字。
+	 *限制：
+	 * 0 <= 节点个数 <= 5000
+	 * 例如，给出
+	 * 前序遍历 preorder = [3,9,20,15,7]
+	 * 中序遍历 inorder = [9,3,15,20,7]
+	 * 返回如下的二叉树：
+	 *     3
+	 *    / \
+	 *   9  20
+	 *     /  \
+	 *    15   7
+	 */
+	public TreeNode buildTree(int[] preorder, int[] inorder) {
+		return null;
+	}
+}
---
+++ src/main/java/com/zjf/algorithm/offer/TreeNode.java
@@ -0,0 +1,17 @@
+package com.zjf.algorithm.offer;
+
+/**
+ * @Description :
+ * @Author : ZJF
+ * @Date: 2020-10-19 22:29  //时间
+ */
+public class TreeNode {
+
+	int val;
+	TreeNode left;
+	TreeNode right;
+
+	TreeNode(int x) {
+		val = x;
+	}
+}
