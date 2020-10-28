9de48fa7c3bb1914904552d73e9f8bb4b3a36923
张建峰 <736391262@qq.com>
2020-10-28 17:00:08 +0800

    力扣 offer13

---
+++ src/main/java/com/zjf/algorithm/offer/Offer16.java
@@ -0,0 +1,33 @@
+package com.zjf.algorithm.offer;
+
+/**
+ * description: Offer16 <br>
+ * date: 2020/10/28 16:54 <br>
+ * author: 张建峰 <br>
+ */
+public class Offer16 {
+	/**
+	 * 实现函数double Power(double base, int exponent)，求base的exponent次方。
+	 * 不得使用库函数，同时不需要考虑大数问题。
+	 *
+	 *
+	 * 示例 1:
+	 * 输入: 2.00000, 10
+	 * 输出: 1024.00000
+	 * 示例 2:
+	 * 输入: 2.10000, 3
+	 * 输出: 9.26100
+	 * 示例 3:
+	 * 输入: 2.00000, -2
+	 * 输出: 0.25000
+	 * 解释: 2-2 = 1/22 = 1/4 = 0.25
+	 *
+	 * 说明:
+	 * -100.0 < x < 100.0
+	 * n 是 32 位有符号整数，其数值范围是 [−231, 231 − 1] 。
+	 *
+	*/
+	public double myPow(double x, int n) {
+		return 2.1;
+	}
+}
