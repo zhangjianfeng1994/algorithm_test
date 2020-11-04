7e0af7bf3c0d2e6897a9e13a3f332268d0f19ec5
zhangjianfeng <zhangjianfeng_sl@163.com>
2020-11-05 00:22:11 +0800

    Offer17

--- src/main/java/com/zjf/algorithm/offer/Offer17.java
+++ src/main/java/com/zjf/algorithm/offer/Offer17.java
@@ -19,6 +19,94 @@ public class Offer17 {
 	 * n 为正整数
 	 */
 	public int[] printNumbers(int n) {
-		return new int[2];
+		int end = (int)Math.pow(10, n) - 1;
+		int[] res = new int[end];
+		for(int i = 0; i < end; i++) {
+			res[i] = i + 1;
+		}
+		return res;
 	}
+
+	/**
+	 *1.由于存在大数问题，结果不能放入int数组中，故这里采用剑指offer原题的直接打印输出的模式。
+	 * 2.increment函数，若发生进位则一直进行for循环，直到不产生进位则break。
+	 * 如果i为0（即到了最高位）还发生了进位，则设置isOverflow为true，并返回至主函数的while判断。
+	 *
+	 */
+	public void printNumbers1(int n) {
+		StringBuilder str = new StringBuilder();
+		// 将str初始化为n个'0'字符组成的字符串
+		for (int i = 0; i < n; i++) {
+			str.append('0');
+		}
+		while(!increment(str)){
+			// 去掉左侧的0
+			int index = 0;
+			while (index < str.length() && str.charAt(index) == '0'){
+				index++;
+			}
+			System.out.println(str.toString().substring(index));
+		}
+	}
+
+	public boolean increment(StringBuilder str) {
+		boolean isOverflow = false;
+		for (int i = str.length() - 1; i >= 0; i--) {
+			char s = (char)(str.charAt(i) + 1);
+			// 如果s大于'9'则发生进位
+			if (s > '9') {
+				str.replace(i, i + 1, "0");
+				if (i == 0) {
+					isOverflow = true;
+				}
+			}
+			// 没发生进位则跳出for循环
+			else {
+				str.replace(i, i + 1, String.valueOf(s));
+				break;
+			}
+		}
+		return isOverflow;
+	}
+
+
+
+	StringBuilder sb;
+	int idx = 0;
+	public boolean increment(int n){
+		boolean carry=false;
+		for(int i=0;i<sb.length();++i){
+			if(carry || i==0){
+				if(sb.charAt(i)=='9'){
+					sb.setCharAt(i,'0');
+					carry = true;
+				}else{
+					sb.setCharAt(i,(char) (sb.charAt(i)+1));
+					carry = false;
+				}
+			}else{
+				break; // no addition on last idx, no need to compute any more
+			}
+		}
+		if(carry){
+			sb.append("1");
+		}
+		return sb.length()<=n; // overflow!
+	}
+
+	public void save(int ans[]){
+		ans[idx] = Integer.parseInt(sb.reverse().toString());
+		sb.reverse();
+	}
+
+	public int[] printNumbers2(int n) {
+		int[] ans = new int[(int) Math.pow(10,n) - 1];
+		sb = new StringBuilder("0");
+		while(increment(n)){
+			save(ans);
+			idx++;
+		}
+		return ans;
+	}
+
 }
