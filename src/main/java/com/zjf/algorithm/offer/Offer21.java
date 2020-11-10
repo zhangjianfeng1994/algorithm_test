24252dad97bcb9c512d2a490495b3ed97c4aed0e
zhangjianfeng <zhangjianfeng_sl@163.com>
2020-11-11 00:23:05 +0800

    Offer21

--- src/main/java/com/zjf/algorithm/offer/Offer21.java
+++ src/main/java/com/zjf/algorithm/offer/Offer21.java
@@ -6,4 +6,23 @@ package com.zjf.algorithm.offer;
  * @Date: 2020-11-10 00:03  //时间
  */
 public class Offer21 {
+
+	/**
+	 * 输入一个整数数组，实现一个函数来调整该数组中数字的顺序，
+	 * 使得所有奇数位于数组的前半部分，所有偶数位于数组的后半部分。
+	 *
+	 *
+	 * 示例：
+	 * 输入：nums = [1,2,3,4]
+	 * 输出：[1,3,2,4]
+	 * 注：[3,1,2,4] 也是正确的答案之一。
+	 *
+	 * 提示：
+	 * 1 <= nums.length <= 50000
+	 * 1 <= nums[i] <= 10000
+	 */
+	//双指针交换
+	public int[] exchange(int[] nums) {
+		return nums;
+	}
 }
