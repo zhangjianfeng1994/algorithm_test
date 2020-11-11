1657e22755151a25be02d5d61574b9227d2f9708
张建峰 <736391262@qq.com>
2020-11-11 18:55:34 +0800

    力扣 Offer21

--- src/main/java/com/zjf/algorithm/offer/Offer21.java
+++ src/main/java/com/zjf/algorithm/offer/Offer21.java
@@ -1,5 +1,7 @@
 package com.zjf.algorithm.offer;
 
+import java.util.Arrays;
+
 /**
  * @Description :
  * @Author : ZJF
@@ -23,6 +25,32 @@ public class Offer21 {
 	 */
 	//双指针交换
 	public int[] exchange(int[] nums) {
+		if (nums.length == 0){
+			return nums;
+		}
+		int left = 0;
+		int right = nums.length-1;
+		while (left != right){
+			//奇数
+			while ((nums[left]&1) ==1 && left < right ){
+				left++;
+			}
+			//偶数
+			while ((nums[right]&1) !=1 && left < right){
+				right--;
+			}
+			if (left < right){
+				int tmp = nums[left];
+				nums[left] = nums[right];
+				nums[right] = tmp;
+			}
+		}
 		return nums;
+	}
+
+	public static void main(String[] args) {
+		Offer21 test = new Offer21();
+		int[] nums = {1,2,3,4};
+		System.out.println(Arrays.toString(test.exchange(nums)));
 	}
 }
