daf29839e6bfd2601826776504441b2f8bbaaa01
张建峰 <736391262@qq.com>
2020-11-19 16:44:43 +0800

    力扣 Offer28

--- src/main/java/com/zjf/algorithm/offer/Offer28.java
+++ src/main/java/com/zjf/algorithm/offer/Offer28.java
@@ -1,5 +1,10 @@
 package com.zjf.algorithm.offer;
 
+import com.sun.xml.internal.bind.v2.model.core.ID;
+
+import java.util.LinkedList;
+import java.util.Queue;
+
 /**
  * @Description :
  * @Author : ZJF
@@ -34,6 +39,56 @@ public class Offer28 {
 	 *
 	 */
 	public boolean isSymmetric(TreeNode root) {
-		return false;
+		if (root == null){
+			return true;
+		}
+		Queue<TreeNode> queue = new LinkedList<>();
+		queue.add(root.left);
+		queue.add(root.right);
+		while (!queue.isEmpty()){
+			int size = queue.size();
+			for (int i = 0; i < size/2; i++) {
+				TreeNode left = queue.poll();
+				TreeNode right = queue.poll();
+				if (left == null && right != null){
+					return false;
+				}
+				if (left != null && right == null){
+					return false;
+				}
+				if (left != null && right != null){
+					if (left.val != right.val){
+						return false;
+					}
+					queue.offer(left.left);
+					queue.offer(right.right);
+					queue.offer(left.right);
+					queue.offer(right.left);
+				}
+			}
+		}
+		return true;
+	}
+
+	public boolean isSymmetric1(TreeNode root) {
+		if (root == null){
+			return true;
+		}
+		return isSame(root.left,root.right);
+	}
+	public boolean isSame(TreeNode left,TreeNode right){
+		if (left == null && right != null){
+			return false;
+		}
+		if (left != null && right == null){
+			return false;
+		}
+		if (left == null && right == null){
+			return true;
+		}
+		if (left.val != right.val){
+			return false;
+		}
+		return isSame(left.left,right.right) && isSame(left.right,right.left);
 	}
 }
