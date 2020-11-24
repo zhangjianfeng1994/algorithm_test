d247020d0baaa538fd87cab71827b5995868919d
张建峰 <736391262@qq.com>
2020-11-24 19:44:19 +0800

    力扣 Offer32_1

---
+++ src/main/java/com/zjf/algorithm/offer/Offer32_1.java
@@ -0,0 +1,28 @@
+package com.zjf.algorithm.offer;
+
+/**
+ * description: Offer31_1 <br>
+ * date: 2020/11/24 19:39 <br>
+ * author: 张建峰 <br>
+ */
+public class Offer32_1 {
+
+	/**
+	 * 从上到下打印出二叉树的每个节点，同一层的节点按照从左到右的顺序打印。
+	 *
+	 *  
+	 *
+	 * 例如:
+	 * 给定二叉树: [3,9,20,null,null,15,7],
+	 *     3
+	 *    / \
+	 *   9  20
+	 *     /  \
+	 *    15   7
+	 * 返回：
+	 * [3,9,20,15,7]
+	*/
+	public int[] levelOrder(TreeNode root) {
+		return new int[1];
+	}
+}
