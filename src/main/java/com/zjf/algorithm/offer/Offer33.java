8d4c29a7843440deed060f335a4e41e086536acc
张建峰 <736391262@qq.com>
2020-11-27 18:42:14 +0800

    力扣 Offer33

---
+++ src/main/java/com/zjf/algorithm/offer/Offer33.java
@@ -0,0 +1,31 @@
+package com.zjf.algorithm.offer;
+
+/**
+ * description: Offer33 <br>
+ * date: 2020/11/27 18:36 <br>
+ * author: 张建峰 <br>
+ */
+public class Offer33 {
+
+	/**
+	 * 输入一个整数数组，判断该数组是不是某二叉搜索树的后序遍历结果。如果是则返回 true，否则返回 false。假设输入的数组的任意两个数字都互不相同。
+	 *
+	 *  
+	 *
+	 * 参考以下这颗二叉搜索树：
+	 *
+	 *      5
+	 *     / \
+	 *    2   6
+	 *   / \
+	 *  1   3
+	 * 示例 1：
+	 *
+	 * 输入: [1,6,3,2,5]
+	 * 输出: false
+	 *
+	*/
+	public boolean verifyPostorder(int[] postorder) {
+		return false;
+	}
+}
