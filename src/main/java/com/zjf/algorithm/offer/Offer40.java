5d8dc8f2689db90f71aff078b56219d112deabf3
zhangjianfeng <zhangjianfeng_sl@163.com>
2020-12-23 23:24:03 +0800

    Offer40

---
+++ src/main/java/com/zjf/algorithm/offer/Offer40.java
@@ -0,0 +1,25 @@
+package com.zjf.algorithm.offer;
+
+/**
+ * @Description :
+ * @Author : ZJF
+ * @Date: 2020-12-23 23:03  //时间
+ */
+public class Offer40 {
+
+	/**
+	 * 输入整数数组 arr ，找出其中最小的 k 个数。例如，输入4、5、1、6、2、7、3、8这8个数字，则最小的4个数字是1、2、3、4。
+	 * 示例 1：
+	 *
+	 * 输入：arr = [3,2,1], k = 2
+	 * 输出：[1,2] 或者 [2,1]
+	 * 示例 2：
+	 *
+	 * 输入：arr = [0,1,2,1], k = 1
+	 * 输出：[0]
+	 *
+	 */
+	public int[] getLeastNumbers(int[] arr, int k) {
+		return new int[k];
+	}
+}
