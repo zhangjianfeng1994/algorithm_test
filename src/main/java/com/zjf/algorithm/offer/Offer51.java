ca36def8be3e190752009b206f6c5404ff7c0d91
zhangjianfeng <zhangjianfeng_sl@163.com>
2021-01-09 01:27:19 +0800

    Offer51

--- src/main/java/com/zjf/algorithm/offer/Offer51.java
+++ src/main/java/com/zjf/algorithm/offer/Offer51.java
@@ -12,11 +12,108 @@ public class Offer51 {
 	 * 输入一个数组，求出这个数组中的逆序对的总数。
 	 *
 	 * 示例 1:
-	 * 输入: [7,5,6,4]
+	 * 输入: [7,5,6,4] 5:1 6:2
+	 *      7 6 5 4    5:2 6:1
 	 * 输出: 5
 	 * 记录当前的下标,排序之后记录排序之后的下标
 	*/
 	public int reversePairs(int[] nums) {
-		return 1;
+		int count= 0;
+		int len = nums.length;
+		for (int i = 0; i < len-1; i++) {
+			for (int j = i+1; j < len; j++) {
+				if (nums[i]>nums[j]){
+					count++;
+				}
+			}
+		}
+		return count;
 	}
+
+	//归并排序
+	public int reversePairs1(int[] nums) {
+		int len = nums.length;
+
+		if (len < 2) {
+			return 0;
+		}
+
+		int[] copy = new int[len];
+		for (int i = 0; i < len; i++) {
+			copy[i] = nums[i];
+		}
+
+		int[] temp = new int[len];
+		return reversePairs(copy, 0, len - 1, temp);
+	}
+
+	/**
+	 * nums[left..right] 计算逆序对个数并且排序
+	 *
+	 * @param nums
+	 * @param left
+	 * @param right
+	 * @param temp
+	 * @return
+	 */
+	private int reversePairs(int[] nums, int left, int right, int[] temp) {
+		if (left == right) {
+			return 0;
+		}
+
+		int mid = left + (right - left) / 2;
+		int leftPairs = reversePairs(nums, left, mid, temp);
+		int rightPairs = reversePairs(nums, mid + 1, right, temp);
+
+		// 如果整个数组已经有序，则无需合并，注意这里使用小于等于
+		if (nums[mid] <= nums[mid + 1]) {
+			return leftPairs + rightPairs;
+		}
+
+		int crossPairs = mergeAndCount(nums, left, mid, right, temp);
+		return leftPairs + rightPairs + crossPairs;
+	}
+
+	/**
+	 * nums[left..mid] 有序，nums[mid + 1..right] 有序
+	 *
+	 * @param nums
+	 * @param left
+	 * @param mid
+	 * @param right
+	 * @param temp
+	 * @return
+	 */
+	private int mergeAndCount(int[] nums, int left, int mid, int right, int[] temp) {
+		for (int i = left; i <= right; i++) {
+			temp[i] = nums[i];
+		}
+
+		int i = left;
+		int j = mid + 1;
+
+		int count = 0;
+		for (int k = left; k <= right; k++) {
+			// 有下标访问，得先判断是否越界
+			if (i == mid + 1) {
+				nums[k] = temp[j];
+				j++;
+			} else if (j == right + 1) {
+				nums[k] = temp[i];
+				i++;
+			} else if (temp[i] <= temp[j]) {
+				// 注意：这里是 <= ，写成 < 就不对，请思考原因
+				nums[k] = temp[i];
+				i++;
+			} else {
+				nums[k] = temp[j];
+				j++;
+
+				// 在 j 指向的元素归并回去的时候，计算逆序对的个数，只多了这一行代码
+				count += (mid - i + 1);
+			}
+		}
+		return count;
+	}
+
 }
